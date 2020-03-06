86a3cec55900768b3db6bbf6a78fb1a69be6a338
Kaephas Kain <kkain@mail.greenriver.edu>
2020-03-06 13:57:28 -0800

    app.use(header stuff)

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
-app.options('*', cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const PORT = 5000;
 
