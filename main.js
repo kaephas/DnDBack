de36ab01d447bf183b9e0b726b8018fce8a1a877
Kaephas <kkain@mail.greenriver.edu>
2020-03-05 21:47:12 -0800

    swap to esm

---
+++ main.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import routes from './src/routes/routes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://kaephasdnd:btanJ9L7U!xfGR2@ds213896.mlab.com:13896/heroku_qnsktrdp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// body parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.get('/', (req, res) => {
+    res.send(`Node and express running on ${PORT}`);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
