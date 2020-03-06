44fa32a21b07b6bc513b85c3d802e66d8e94243d
Kaephas <kkain@mail.greenriver.edu>
2020-03-05 22:56:53 -0800

    try putting promise back

--- main.js
+++ main.js
@@ -7,28 +7,29 @@ const app = express();
 const PORT = 5000;
 
 // mongoose connection
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; // TODO: is this what broke the app on heroku?
 mongoose.connect('mongodb://kaephasdnd:NiakSahpeak314@ds213896.mlab.com:13896/heroku_qnsktrdp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+// check mongodb connection status
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection issues..."));
 db.once('open', function() {
-    console.log('connected');
+    console.log('connected to db');
 });
 
 // body parser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-
-
 app.get('/', (req, res) => {
     res.send(`Node and express running on ${process.env.PORT || PORT}`);
 });
 
+// api routes
 routes(app);
 
+// start app
 app.listen(process.env.PORT || PORT, () => console.log(`Listening on port ${process.env.PORT || PORT}...`));
