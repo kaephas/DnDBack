07d5184ddb8a1bae9568e7d63837c665c95d42c4
Kaephas <kkain@mail.greenriver.edu>
2020-03-07 13:23:28 -0800

    trying diff options

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
     if('OPTIONS' === req.method) {
         res.send(200);
     } else {
